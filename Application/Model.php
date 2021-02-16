3a0b7c6ad36811dd402d8c53ef2770917b67348b
Fabio TENORIO <fabiovalho@gmail.com>
2021-02-16 17:51:07 +0100

    db connexion works

--- Application/Model.php
+++ Application/Model.php
@@ -11,42 +11,68 @@ CONTIENT L'ACCÈS À LA BASE DE DONNÉES (voir le code que m'avez montré NADIA)
 
 Namespace App\Application;
 
-class Model {
+class Model
+{
+    public function connectDb()
+    {
+        $host = 'localhost';
+        $db = 'boutique';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+    
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try
+        {
+         $pdo = new \PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+         throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
     //le choix des variables plutôt que des constants (DEFINE) a été fait pour raison de sécurité
     // vu que les constants restent disponibles globalement
-    private $db_host;
-    private $db_login;
-    private $db_password;
-    private $db_name;
-    protected $_PDO;
+
+    // Informations de connexion
+    
+   
+    // private $db_host;
+    // private $db_login;
+    // private $db_password;
+    // private $db_name;
+    // protected $_PDO;
     
     //déplacer vers l'intérieur d'une méthode?
-    public $table;
-    public $id;
-    public $msg;
+    // public $table;
+    // public $id;
+    // public $msg;
 
-    public function __construct()
-    {
-        $this->db_host = "localhost";
-        $this->db_login = "root";
-        $this->db_password = '';
-        $this->db_name = "boutique";
-    }
+    // public function __construct()
+    // {
+    //     $this->db_host = "localhost";
+    //     $this->db_login = "root";
+    //     $this->db_password = '';
+    //     $this->db_name = "boutique";
+    // }
 
     public function test() {
         echo "hello number ";
     }
 
-    public function connectDb() {
-        try {
-            $this->_PDO = new \PDO("mysql:dbname=$this->db_name;host=$this->db_host;", $this->db_login, $this->db_password);
-            $this->_PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $this->_PDO->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
-            return $this->_PDO;
-        } catch (\PDOException $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-    }
+    // public function connectDb() {
+    //     try {
+    //         $this->_PDO = new \PDO("mysql:dbname=$this->db_name;host=$this->db_host;", $this->db_login, $this->db_password);
+    //         $this->_PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    //         $this->_PDO->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+    //         return $this->_PDO;
+    //     } catch (\PDOException $e) {
+    //         die('Erreur : ' . $e->getMessage());
+    //     }
+    // }
 
     public function getOne(){
         $sql = "SELECT * FROM ".$this->table." WHERE id=".$this->id;
@@ -79,6 +105,6 @@ class Model {
     //     }
 
     // }
-}
 
+    }
 ?>

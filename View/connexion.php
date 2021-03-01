1dee1f1240bb2909bf2569c9259da94133f54dc2
Fabio TENORIO <fabiovalho@gmail.com>
2021-03-01 01:22:12 +0100

    some style to connexion and inscription

--- View/connexion.php
+++ View/connexion.php
@@ -8,52 +8,21 @@ Use App\Application\Controllers\ControllerAccueil as ControllerAccueil;
 <div id="main_connexion">
 <div id="img-connexion">
 </div>
-<form id="formulaire_connexion" action="<?php echo CONNEXION; ?>" method="GET">
+<form action="<?php echo CONNEXION; ?>" method="GET">
   <h1>connexion</h1>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="email">Email</label>
-      <input type="email" name="email" id="email" class="form-control" placeholder="Email">
+  <div class="form">
+    <div class="form-group row">
+      <label for="mail">Email</label>
+      <input type="email" name="mail" id="mail" class="form-control" placeholder="example@email.com">
     </div>
-    <div class="form-group col-md-6">
-      <label for="motpasse">Password</label>
-      <input type="motpasse" class="form-control" id="motpasse" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="adresse">Address</label>
-    <input type="text" class="form-control" name="adresse" id="adresse" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
+    <div class="form-group row">
+      <label for="motpasse">Mot de passe</label>
+      <input type="motpasse" class="form-control" id="motpasse" placeholder="votre mot de passe">
     </div>
   </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
+    <div class="form-group row">
+        <button type="submit" class="btn btn-primary">Me connecter</button>
     </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
 </form>
 </div>
 <!-- </main> -->
--- View/inscription.php
+++ View/inscription.php
@@ -1,51 +1,59 @@
 <?php
 
 ?>
+<div id="main_inscription">
+    <form id="form_inscription" action="<?php echo INSCRIPTION; ?>" method="POST">
+        <div class="container" id="form_inscription_container">
+            <h1>inscription</h1>
+            <!-- <p>Please fill in this form to create an account.</p> -->
+            <hr>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label for="prenom">Prénom</label>
+                        <input type="text" placeholder="votre prénom" name="prenom" id="prenom" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="nom">Nom</label>
+                        <input type="text" placeholder="votre nom" name="nom" id="nom" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="motpasse">Mot de passe</label>
+                        <input type="password" placeholder="votre mot de passe" name="motpasse" id="motpasse" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="confirmer_motpasse">Confirmez votre mot de passe</label>
+                        <input type="password" placeholder="réecrivez-le ici" name="confirmer_motpasse" id="confirmer_motpasse" required>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <label for="mail">e-mail</label>
+                        <input type="text" placeholder="example@email.com" name="mail" id="mail" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="login">votre pseudo</label>
+                        <input type="text" placeholder="votre pseudo" name="login" id="login" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="telephone">téléphone</label>
+                        <input type="tel" placeholder="votre numéro de téléphone" name="telephone" id="telephone" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="dateanniversaire">date de naissance</label>
+                        <input type="date" placeholder="votre date de naissance" name="dateanniversaire" id="telephone">
+                    </div>
+                </div>
+            </div>
+            <hr>
 
-<form id="form_inscription" action="<?php echo INSCRIPTION; ?>" method="POST">
-  <div class="container">
-    <h1>inscription</h1>
-    <!-- <p>Please fill in this form to create an account.</p> -->
-    <hr>
-    <div>
-        <label for="login"><b>login</b></label>
-        <input type="text" placeholder="votre login" name="login" id="login" required>
-    </div>
-    <div>
-        <label for="motpasse"><b>Mot de passe</b></label>
-        <input type="password" placeholder="votre mot de passe" name="motpasse" id="motpasse" required>
-    </div>
-    <div>
-        <label for="confirmer_motpasse"><b>Mot de passe</b></label>
-        <input type="password" placeholder="confirmez votre mot de passe" name="confirmer_motpasse" id="confirmer_motpasse" required>
-    </div>
-    <div>
-        <label for="prenom"><b>Prénom</b></label>
-        <input type="text" placeholder="votre prénom" name="prenom" id="prenom" required>
-    </div>
-    <div>
-        <label for="nom"><b>Nom</b></label>
-        <input type="text" placeholder="votre nom" name="nom" id="nom" required>
-    </div>
-    <div>
-        <label for="mail"><b>e-mail</b></label>
-        <input type="text" placeholder="votre courrier électronique" name="mail" id="mail" required>
-    </div>
-    <div>
-        <label for="telephone"><b>téléphone</b></label>
-        <input type="tel" placeholder="votre numéro de téléphone" name="telephone" id="telephone" required>    
-    </div>
-    <div>
-        <label for="dateanniversaire"><b>date de naissance</b></label>
-        <input type="date" placeholder="votre date de naissance" name="dateanniversaire" id="telephone">    
-    </div>
-    <hr>
-
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-    <button type="submit" class="btn btn-primary">Register</button>
-  </div>
-
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
-  </div>
-</form>
+            <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+            <button type="submit" class="btn btn-primary">m'inscrire</button>
+            <div class="container signin">
+                <p>Already have an account? <a href="#">Sign in</a>.</p>
+            </div>
+        </div>
+    </form>
+</div>

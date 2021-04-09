6e42935a66ec9890888085090446f9846b0e1396
olivier-puche <71874455+olivier-puche@users.noreply.github.com>
2021-04-09 09:48:13 +0200

    modifs des models

--- Application/Models/ModelAdmin.php
+++ Application/Models/ModelAdmin.php
@@ -46,8 +46,61 @@ class ModelAdmin extends Model {
         return $this->insert('produit', $data);
     }
 
+
+
+
+    
+    public function update_product($data, $id)
+    {
+        return $this->update('produit', $data, $id);
+    }
+
     public function deleteProduct($id) {
         $this->delete('produit', $id);
+    }
+
+
+
+
+
+
+    public function new_fournisseur_data($data)
+    {
+        $insert_values = [];
+        // je crée un array avec les noms des colonnes
+        $column_names = $this->columns_names('fournisseur');
+        // je parcours l'array associatif $data qui contient les données renseignés par ControllerUser
+        foreach ($data as $key => $value)
+        {
+            // je parcours aussi les noms des colonnes
+            for ($i=0;isset($column_names[$i]);$i++)
+            {
+                //afin de vérificer, pour chaque element de $data,
+                //si la clé correspond au nom d'une colonne
+                if ($column_names[$i]==$key)
+                {
+                    //si oui, j'ajoute $value à l'array insert_values
+                    //pour l'envoyer à la méthode insert_values
+                    $insert_values[$key]=$value;
+                }
+            }
+        }
+        return $insert_values;
+    }
+
+    public function insert_fournisseur($data)
+    {
+        $data = $this->new_fournisseur_data($data);
+        return $this->insert('fournisseur', $data);
+    }
+
+    public function update_fournisseur($data, $id)
+    {
+        return $this->update('fournisseur', $data, $id);
+    }
+
+    public function deleteFournisseur($id) {
+        $this->delete('fournisseur', $id);
     }
     
 }

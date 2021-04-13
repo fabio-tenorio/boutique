b7a5a143624f46b2a170ca67499a6b0de4f69fa1
Fabio TENORIO <fabiovalho@gmail.com>
2021-04-14 00:23:40 +0200

    bugs in methods verifierStock, panier, and commande solved. It was a problem concerning datatypes compatibility with the method verifierStock

--- Application/Controllers/ControllerProduits.php
+++ Application/Controllers/ControllerProduits.php
@@ -37,10 +37,11 @@ class ControllerProduits extends Controller
     public function verifierStock($produits) {
         $stock = $this->allPRoduits();
         $checkStock = true;
-        foreach($produits as $produit) {
+        foreach($produits as $produitDemande) {
             foreach($stock as $produitDispo) {
-                if ($produit->id == $produitDispo->id) {
-                    if ($produitDispo->stock - $produit->quantite < 0) {
+                // var_dump($produits);
+                if ($produitDemande->id == $produitDispo->id) {
+                    if ($produitDispo->stock - $produitDemande->quantite < 0) {
                         $this->message = "Désolé. On n'est dispose pas de cette quantité de produits";
                         $checkStock = false;
                     } else {
@@ -62,27 +63,38 @@ class ControllerProduits extends Controller
 
     public function panier () {
         if (isset($_POST['updatepanier']) and isset($_SESSION['panier'])) {
-            $panier = $_POST;
-            unset($panier['updatepanier']);
+            $panier = array();
+            $index = 0;
+            foreach($_POST as $key=>$value) {
+                $panier[$index]['id'] = $key;
+                $panier[$index]['quantite'] = $value;
+                $panier[$index]=(object)$panier[$index];
+                $index++;
+            }
+            // var_dump($panier);
             if ($this->verifierStock($panier) === true) {
                 $this->panierTotal = array();
                 foreach($_SESSION['panier'] as $produit) {
-                    foreach($panier as $produitId => $quantite) {
-                        if ($produit->id == $produitId) {
-                            $produit->quantite = $quantite;
-                            $this->panierTotal[$produit->id] = $this->calculerSousTotal($produit->prix, $quantite);
+                    foreach($panier as $produitPost) {
+                        if ($produit->id == $produitPost->id) {
+                            $produit->quantite = $produitPost->quantite;
+                            $this->panierTotal[$produit->id] = $this->calculerSousTotal($produit->prix, $produitPost->quantite);
+                            $this->panierTotal[$produit->id] = number_format($this->panierTotal[$produit->id], 2, '.', ',');
                         }
                     }
                 }
                 $this->total = array_sum($this->panierTotal);
+                $this->total = number_format($this->total, 2, '.', ',');
                 $this->render('panier');
             } else {
                 $panier = $_SESSION['panier'];
                 $this->panierTotal = array();
                 foreach($panier as $produit) {
                     $this->panierTotal[$produit->id] = $this->calculerSousTotal($produit->prix, $produit->quantite);
+                    $this->panierTotal[$produit->id] = number_format($this->panierTotal[$produit->id], 2, '.', ',');
                 }
                 $this->total = array_sum($this->panierTotal);
+                $this->total = number_format($this->total, 2, '.', ',');
                 $this->render('panier');
             }
         } elseif (!isset($_POST['updatepanier']) and isset($_SESSION['panier'])) {
@@ -90,8 +102,10 @@ class ControllerProduits extends Controller
             $this->panierTotal = array();
             foreach($panier as $produit) {
                 $this->panierTotal[$produit->id] = $this->calculerSousTotal($produit->prix, $produit->quantite);
+                $this->panierTotal[$produit->id] = number_format($this->panierTotal[$produit->id], 2, '.', ',');
             }
             $this->total = array_sum($this->panierTotal);
+            $this->total = number_format($this->total, 2, '.', ',');
             $this->render('panier');
         } else {
             $_SESSION['panier'] = array ();
@@ -138,8 +152,10 @@ class ControllerProduits extends Controller
             $this->panierTotal = array();
             foreach($this->panier as $produit) {
                 $this->panierTotal[$produit->id] = $this->calculerSousTotal($produit->prix, $produit->quantite);
+                $this->panierTotal[$produit->id] = number_format($this->panierTotal[$produit->id], 2, '.', ',');
             }
             $this->total = array_sum($this->panierTotal);
+            $this->total = number_format($this->total, 2, '.', ',');
         } else {
             $_SESSION['panier'] = array ();
             $this->panierTotal = array ();
@@ -151,6 +167,10 @@ class ControllerProduits extends Controller
         //     $this->client = array();
         //     var_dump($this->client);
         // }
+    }
+
+    public function updateLigneCommande($produits) {
+                
     }
 
     public function updateStock($produitsPanier) {
@@ -172,20 +192,26 @@ class ControllerProduits extends Controller
             $this->message = '<h1 class="my-5 text-center">Votre commande était vide</h1><h2 class="my-5 text-center">Aucun prélèvement a eu lieu</h2>';
             return $this->render('commandeconfirmation');
         } else {
+            foreach($this->panier as $key=>$value) {
+                if ($key == 'quantite') {
+                    $data[$key] = $value;
+                }
+            }
             if ($this->verifierStock($this->panier)===true) {
-                $this->updateStock($this->panier);
+                $total = str_replace('.', '', $this->total);
                 require './vendor/autoload.php';
                 \Stripe\Stripe::setApiKey('sk_test_51If0n8GAVfFRDcyqyt7RWuzbgilod4eaJWk85bXVg8xTv1nu4XqTB0qgcfdtuINm84D5Jox1VsGdhhQe2D6XcOJu005WXivVx0');
                 $this->data = filter_var_array($_POST, FILTER_SANITIZE_STRING);
                 $token = $this->data['stripeToken'];
                 $charge = \Stripe\Charge::create([
-                'amount' => 999,
-                'currency' => 'usd',
-                'description' => 'Example charge',
+                'amount' => $total,
+                'currency' => 'eur',
+                'description' => 'description',
                 'source' => $token,
                 ]);
                 $this->charge = $charge;
                 if ($this->charge->outcome->seller_message == "Payment complete.") {
+                    $this->updateStock($this->panier);
                     $this->message = '<h1 class="text-center">Paiement confirmé</h1><h2 class="text-center">Merci de votre visite!</h2>';
                     $this->render('commandeconfirmation');
                 } else {
